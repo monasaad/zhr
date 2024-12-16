e5bd90129b351012ab9c20d72ab291ca832d39bd
monasaad <monaaa.saad@yahoo.com>
2024-12-16 10:21:47 +0300

    added tabview, health screen, change the acent color.

---
+++ zhr/HealthModel.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  zhr
+//
+//  Created by Mona on 15/12/2024.
+//
+
---
+++ zhr/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  zhr
+//
+//  Created by Mona on 16/12/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
