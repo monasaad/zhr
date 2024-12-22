08593cb4d43a465230128407b9c8bd1ddc0df9d7
Huda-slo <huda1slo31@yahoo.com>
2024-12-22 10:50:06 +0300

    huda

---
+++ zhrWatch Watch App/zhrWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  zhrWatchApp.swift
+//  zhrWatch Watch App
+//
+//  Created by Huda Almadi on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct zhrWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
