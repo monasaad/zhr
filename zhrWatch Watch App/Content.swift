daeaaec68231e755afb0aba221c8948c956529ac
jumana <jumana.alyahya@outlook.com>
2025-01-01 12:48:27 +0300

    last

--- zhrWatch Watch App/Content.swift
+++ zhrWatch Watch App/Content.swift
@@ -1,31 +1,8 @@
-import SwiftUI
-import MapKit
+//
+//  Content.swift
+//  zhrWatch Watch App
+//
+//  Created by Jumana S on 01/01/2025.
+//
 
-struct Content: View {
-    @StateObject private var locationManager = WatchLocationManager()
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-    )
-
-    var body: some View {
-        VStack {
-            if let location = locationManager.currentLocation {
-                Map(coordinateRegion: $region, showsUserLocation: true)
-                    .onAppear {
-                        updateRegion(for: location)
-                    }
-                    .onChange(of: location) { newLocation in
-                        updateRegion(for: newLocation)
-                    }
-            } else {
-                Text("Waiting for location...")
-                    .padding()
-            }
-        }
-    }
-
-    private func updateRegion(for location: CLLocation) {
-        region.center = location.coordinate
-    }
-}
+import Foundation
